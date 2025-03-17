5ee14f7dfd3bbb478c1616335178db76d94ccebf
Martien Dermawan Tanama <martiendt@gmail.com>
2025-03-17 13:10:40 +0700

    x

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,8 +3,8 @@ export interface ICorsConfig {
   credentials: boolean
 }
 
-export const origin = process.env['CORS_ORIGIN'] ?? ''
-export const credentials: boolean = process.env['CORS_CREDENTIALS'] === 'true' ? true : false
+export const origin = `${process.env['CORS_ORIGIN']}`
+export const credentials = process.env['CORS_CREDENTIALS'] === 'true' ? true : false
 
 const corsConfig: ICorsConfig = {
   origin,
