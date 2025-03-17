d0b64d458b01cb312f4237388ae1481035837d65
Martien Dermawan Tanama <martiendt@gmail.com>
2025-03-17 12:47:33 +0700

    x

--- src/modules/health/controllers/index.ts
+++ src/modules/health/controllers/index.ts
@@ -1,6 +1,7 @@
 import type { IController, IControllerInput } from '@point-hub/papi'
 
 import { version } from '@/../package.json'
+import apiConfig from '@/config/api'
 import corsConfig from '@/config/cors'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,6 +13,7 @@ export const healthController: IController = async (controllerInput: IController
       status: 'healthy',
       timestamp: new Date(),
       cors: corsConfig.origin,
+      api: apiConfig.baseUrl,
     },
   }
 }
